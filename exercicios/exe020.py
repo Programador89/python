36d38c3c35f19a6036c4791d1c96a1e7fa3ce2c3
Rafael Dantas <rafaeldantasgomes89@gmail.com>
2024-04-05 03:08:37 -0300

    Adicionando os ultimos exercicios

---
+++ exercicios/exe020.py
@@ -0,0 +1,4 @@
+"""
+    O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalho dos alunos. 
+    Faça um programa que leia o nome dos 4 alunos e mostre a ordem sorteada. 
+"""
