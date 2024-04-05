583406a0c68bbb68bfbb078f7c18cfd23e172fdb
Rafael Dantas <rafaeldantasgomes89@gmail.com>
2024-04-05 03:56:11 -0300

    Adicionando recursos no git

--- exercicios/exe018.py
+++ exercicios/exe018.py
@@ -1,4 +1,13 @@
 """
     Faça um programa que leia um angulo qualquer e mostra na tela o valor do: Seno, Cosseno, tangente desse angulo.
 """
+import math
 
+ang = float(input('Informe um angulo: '))
+rad = math.radians(ang)
+
+sen = math.sin(rad)
+cos = math.cos(rad)
+tan = math.tan(rad)
+
+print('O valor do seno do angulo informado é {:.2f}'.format(sen))
